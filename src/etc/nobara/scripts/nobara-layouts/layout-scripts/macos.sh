8e00688931d3df9b92d5792b5119d28f7899c5bb
Ward Nakchbandi <hotrod.master@hotmail.com>
2022-09-24 11:36:34 +0300

    commit

--- src/etc/nobara/scripts/nobara-layouts/layout-scripts/macos.sh
+++ src/etc/nobara/scripts/nobara-layouts/layout-scripts/macos.sh
@@ -4,8 +4,11 @@
 /etc/nobara/scripts/nobara-layouts/layout-scripts/reset.sh
 
 gnome-extensions reset dash-to-dock@micxgx.gmail.com
+gnome-extensions reset Move_Clock@rmy.pobox.com
+
 
 dconf reset -f /org/gnome/shell/extensions/dash-to-dock/
+dconf reset -f /org/gnome/shell/extensions/Move_Clock/
 
 # Get needed extensions into userland
 
@@ -21,6 +24,18 @@ else
 	export RELOG_NEEDED=1
 fi
 
+# Frippery Move Clock
+
+if [ -d "$HOME/.local/share/gnome-shell/extensions/Move_Clock@rmy.pobox.com" ]; then
+	echo "Frippery Move Clock already in userland no need to download anything"
+else
+	mkdir -p "$HOME/.cache/nobara-layouts/extensions/"
+	cd "$HOME/.cache/nobara-layouts/extensions/"
+	ls ./Move_Clockrmy.pobox.com.v27.shell-extension.zip || wget  https://extensions.gnome.org/extension-data/Move_Clockrmy.pobox.com.v27.shell-extension.zip
+	gnome-extensions install "$HOME/.cache/nobara-layouts/extensions/Move_Clockrmy.pobox.com.v27.shell-extension.zip"
+	export RELOG_NEEDED=1
+fi
+
 if [[ "$RELOG_NEEDED" == 1 ]]; then
 	if zenity --question --text="New extensions have been installed!, reload required!"
 	then
@@ -30,4 +45,4 @@ fi
 
 # Enable Dash
 gnome-extensions enable dash-to-dock@micxgx.gmail.com
-
+gnome-extensions enable Move_Clock@rmy.pobox.com
