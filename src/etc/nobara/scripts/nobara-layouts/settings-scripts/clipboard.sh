3a188a6648518bed96f6894b3e17c9aa135e82e6
Ward Nakchbandi <hotrod.master@hotmail.com>
2022-09-26 19:27:10 +0300

    commit

--- src/etc/nobara/scripts/nobara-layouts/settings-scripts/clipboard.sh
+++ src/etc/nobara/scripts/nobara-layouts/settings-scripts/clipboard.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 if [[ $1 == enable ]]; then
-	gnome-extensions enable gnome-clipboard@b00f.github.io
+	gnome-extensions enable clipboard-history@alexsaveau.dev
 	fi
 else
-gnome-extensions disable gnome-clipboard@b00f.github.io
+gnome-extensions disable clipboard-history@alexsaveau.dev
 fi
