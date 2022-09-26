9620192fe08e025eccf4e81abc76b4893a23e85b
Ward Nakchbandi <hotrod.master@hotmail.com>
2022-09-26 19:30:35 +0300

    commit

--- src/etc/nobara/scripts/nobara-layouts/settings-scripts/audio.sh
+++ src/etc/nobara/scripts/nobara-layouts/settings-scripts/audio.sh
@@ -2,6 +2,7 @@
 
 if [[ $1 == enable ]]; then
 	gnome-extensions enable volume-mixer@evermiss.net
+	gnome-extensions enable sound-output-device-chooser@kgshank.net
 else
 gnome-extensions disable volume-mixer@evermiss.net
 gnome-extensions disable sound-output-device-chooser@kgshank.net
