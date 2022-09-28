b5f4f2c17a5c0acda93bec0b19a8e4c1558aca46
Ward Nakchbandi <hotrod.master@hotmail.com>
2022-09-28 21:08:05 +0300

    commit

--- src/etc/nobara/scripts/nobara-layouts/layout-scripts/win11.sh
+++ src/etc/nobara/scripts/nobara-layouts/layout-scripts/win11.sh
@@ -2,13 +2,22 @@
 
 # Reset everything
 
-gnome-extensions reset dash-to-panel@jderose9.github.com || echo
-gnome-extensions reset arcmenu@arcmenu.com || echo
-gnome-extensions reset just-perfection-desktop@just-perfection || echo
+# Reset everything
 
+#
+dconf reset -f /org/gnome/shell/extensions/arcmenu/ || echo
+dconf reset -f /org/gnome/shell/extensions/dash-to-dock/ || echo
 dconf reset -f /org/gnome/shell/extensions/dash-to-panel/ || echo
 dconf reset -f /org/gnome/shell/extensions/just-perfection/ || echo
-dconf reset -f /org/gnome/shell/extensions/arcmenu/ || echo
+dconf reset -f /org/gnome/shell/extensions/Move_Clock/ || echo
+gnome-extensions reset arcmenu@arcmenu.com || echo
+gnome-extensions reset dash-to-dock@micxgx.gmail.com || echo
+gnome-extensions reset dash-to-panel@jderose9.github.com || echo
+gnome-extensions reset just-perfection-desktop@just-perfection || echo
+gnome-extensions reset launch-new-instance@gnome-shell-extensions.gcampax.github.com || echo
+gnome-extensions reset Move_Clock@rmy.pobox.com || echo
+gnome-extensions reset places-menu@gnome-shell-extensions.gcampax.github.com || echo
+gnome-extensions reset window-list@gnome-shell-extensions.gcampax.github.com || echo
 
 # add new settings
 gsettings set org.gnome.shell.extensions.arcmenu menu-hotkey 'Super_L'
