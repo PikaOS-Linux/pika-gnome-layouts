ff728f9165ffa6e39fcb6c4fd0ce11fea5f74672
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-03-27 00:37:34 +0300

    Redo Themeing system

--- usr/lib/pika/gnome-layouts/libadwaita.sh
+++ usr/lib/pika/gnome-layouts/libadwaita.sh
@@ -1,27 +1,25 @@
 #! /bin/bash
+
+# Remove current libadwaita theme
 rm -rfv $HOME/.config/gtk-4.0
-THEME=$(dconf read /org/gnome/desktop/interface/gtk-theme | sed "s+'++" | sed "s+'++" | sed "s+-Dark++" | sed "s+-Light++" )
-if echo $THEME | grep -i orchis
-    then
-    if [[ $1 = dark ]]
-    then
-        kvantummanager --set Orchis-dark
-        cp -rfv /usr/share/themes/$THEME-Dark/gtk-4.0 $HOME/.config/
-    else
-        kvantummanager --set Orchis
-        cp -rfv /usr/share/themes/$THEME-Light/gtk-4.0 $HOME/.config/
-    fi
+
+# Import Theme Settings From system dir
+if [ -f /etc/pika_theme ]
+then
+. "/etc/pika_theme"
+fi
+
+# Import Theme Settings From user dir
+if [ -f $HOME/.config/pika_theme ]
+then
+. "$HOME/.config/pika_theme"
+fi
+
+if [[ $1 = dark ]]
+then
+kvantummanager --set $KVANTUM_THEME_DARK
+cp -rfv $GTK_THEME_PATH_DARK/gtk-4.0 $HOME/.config/
 else
-    if [ -f  $HOME/.config/pika_theme ]
-    . "$HOME/.config/pika_theme"
-    then
-        if [[ $1 = dark ]]
-        then
-            kvantummanager --set $KVANTUM_THEME_DARK
-            cp -rfv $GTK_THEME_PATH_DARK/gtk-4.0 $HOME/.config/
-        else
-            kvantummanager --set $KVANTUM_THEME_LIGHT
-            cp -rfv $GTK_THEME_PATH_LIGHT/gtk-4.0 $HOME/.config/
-        fi
-    fi
+kvantummanager --set $KVANTUM_THEME_LIGHT
+cp -rfv $GTK_THEME_PATH_LIGHT/gtk-4.0 $HOME/.config/
 fi
