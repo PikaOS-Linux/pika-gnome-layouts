e88f666ae3dade12a42868bfee1549f33177e461
ferrreo <harderthanfire@gmail.com>
2023-08-30 20:27:26 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -5,10 +5,10 @@ dpkg-sig --sign builder ./output/pika-gnome-layouts*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Remove our existing package from the repo
-reprepro -V --basedir ./output/repo/ removefilter lunar 'Package (% pika-gnome-layouts*)'
+reprepro -V -C main --basedir ./output/repo/ removefilter lunar 'Package (% pika-gnome-layouts*)'
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-gnome-layouts*.deb
+reprepro -V -C main --basedir ./output/repo/ includedeb lunar ./output/pika-gnome-layouts*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
